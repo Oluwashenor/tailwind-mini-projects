978e7a2d48babc830fdc1f1118dfc05c17e4999e
Adesina <adeshiname@gmail.com>
2023-10-30 12:09:18 +0000

    newsletter

---
+++ newsletter/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'dark-slate-grey': '#1E3A4C', // Dark Slate Grey: hsl(234, 29%, 20%)
+          'charcoal-grey': '#2E3E42',   // Charcoal Grey: hsl(235, 18%, 26%)
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
